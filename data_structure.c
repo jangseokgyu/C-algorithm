d8b8c50fb32392467ba9b1c993f022e4c88d056c
jangseokgyu <43026098+jangseokgyu@users.noreply.github.com>
2019-01-09 19:41:00 +0900

    Update data_structure.c

--- data_structure.c
+++ data_structure.c
@@ -1,17 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct node {
+typedef struct fuck {
   int val;
-  struct node *next;
-}nod;
+  struct fuck *next;
+}fuckyou;
 
 int main() {
-  nod a = {12,0}, b = {10,0};
-  nod *head = &a;
+  fuckyou a = {12,0};
+  fuckyou b = {14,0};
+
   a.next = &b;
 
-  a.next->val = 12;
+  a.next->val = 16;
 
+  printf("%d",a.next->val);
+
+  int *as = malloc(sizeof(int)*4);
+  as[0] = 1;
+  as[1] = 2;
+  as[2] = 3;
+  as[3] = 4;
+
+  printf("\n%d",*(as+2));
   return 0;
 }
